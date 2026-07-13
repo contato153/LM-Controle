c4de784f84497bf3adbc0b6357d23d359058035b
contato153 <contato@contabillm.com.br>
2026-07-13 11:08:26 -0300

    Feat: configura Tailwind CSS v4 localmente e resolve aviso de CDN no console

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import { ToastProvider } from './contexts/ToastContext';
 import { AuthProvider } from './contexts/AuthContext';
